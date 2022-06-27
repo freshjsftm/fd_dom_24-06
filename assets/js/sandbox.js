9d7331d3f451dae610f6a28811bb8205160700b0
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-27 15:48:20 +0300

    Add counter v2.0

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -2,16 +2,20 @@
 
 function createCounter(initial) {
   let i = initial;//closure
-  return function counter() {
-    i++;
-    return i;
-  };
+  return {
+    increment(){
+      return ++i;
+    },
+    decrement(){
+      return --i;
+    }
+  }
 }
-
+let i = 44;
 const counter1 = createCounter(10);
 //debugger
-counter1()
-counter1()
-console.log(counter1())
+counter1.increment()
+counter1.increment()
+console.log(counter1.increment())
 
 const counter2 = createCounter(100);
