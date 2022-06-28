fa117dfa3cb5956595a96532f2adfb6b6640a2a8
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-28 15:21:04 +0300

    Example stopPropagation & capture & once

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,27 +2,21 @@
 
 const test = document.getElementById('test')
 
-const typeBtn = document.createAttribute('type')
-typeBtn.value = 'submit';
-test.setAttributeNode(typeBtn);
 
-const btnHandler = ({target, currentTarget}) =>{
+const btnHandler = (e) =>{
+  //e.cancelBubble = true;
+  e.stopPropagation();
   console.group()
-  typeBtn.value = 'reset'; 
-  console.log(target.setAttribute('title', '123'))
-  console.log(target.innerText)
-  console.log(target.textContent)
-  console.log(target)//на ком произошло событие
-  console.log(currentTarget)//чей обработчик отработал
+  console.log(e.target)//на ком произошло событие
+  console.log(e.currentTarget)//чей обработчик отработал
   console.groupEnd();
 }
-
-test.addEventListener('click', btnHandler);
+test.addEventListener('click', ()=>{console.log(1111)}, {capture:true, once:true});
+test.addEventListener('click', btnHandler, {capture:true});
 window.addEventListener('click', btnHandler);
-document.addEventListener('click', btnHandler);
+document.addEventListener('click', btnHandler, {capture:true});
 document.body.addEventListener('click', btnHandler);
 
-//найти свойство, которое возвращает содержимое элемента
 
 
 
