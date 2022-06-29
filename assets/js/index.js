40051c65ecde1a8c51d24ade4f45485cabcfd0fc
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-29 14:08:29 +0300

    Add DATA actors

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,3 @@
 "use strict";
 
-const [openBtn, closeBtn] = document.querySelectorAll(".btn");
-
-const btnHandler = () => {
-  let buffer = openBtn.textContent;
-  openBtn.textContent = closeBtn.textContent;
-  closeBtn.textContent = buffer;
-};
-
-openBtn.addEventListener("click", btnHandler);
-closeBtn.addEventListener("click", btnHandler);
 
