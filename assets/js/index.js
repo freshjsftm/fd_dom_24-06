4fa007c6c331ba115e3c456d0db190f68dd5f072
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-06-30 15:09:32 +0300

    Fix bugs v2.0

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,69 +1,90 @@
 "use strict";
-const cardsContainer = document.getElementById('cards-container');
+const cardsContainer = document.getElementById("cards-container");
 
 const HTMLCollectionActors = actors
-  .filter((actor)=>actor.name||actor.photo)
-  .map((actor)=>createActorsCard(actor));
+  .filter((actor) => actor.name || actor.photo)
+  .map((actor) => createActorsCard(actor));
 
-function createActorsCard(actor){
-  const {id, name, birthdate, photo} = actor;
+function createActorsCard(actor) {
+  const { id, name, birthdate, photo } = actor;
 
+  const cardName = createElement(
+    "h2",
+    { classNames: ["card-name"] },
+    document.createTextNode(name || "Anonim")
+  );
+
+  const cardInfo = createElement(
+    "p",
+    { classNames: ["card-info"] },
+    document.createTextNode(birthdate)
+  );
+
+  const cardContainer = createElement(
+    "article",
+    { classNames: ["card-container"] },
+    createWrapperPhoto(actor),
+    cardName,
+    cardInfo
+  );
   createImage(actor);
-  const cardPhotoWrapper = createElement('div',{
-    classNames:['card-photo-wrapper'],
-    attributes:{'id':`photo-wrapper${id}`}
-  }, createInitials(actor));
 
-  const cardName = createElement('h2',{classNames:['card-name']}, document.createTextNode(name || 'Anonim') );
-
-  const cardInfo = createElement('p',{classNames:['card-info']}, document.createTextNode(birthdate));
-
-  const cardContainer = createElement('article',{classNames:['card-container']},cardPhotoWrapper, cardName, cardInfo)
-
-  const HTMLCard = createElement('li',{classNames:['card-wrapper']}, cardContainer);
+  const HTMLCard = createElement(
+    "li",
+    { classNames: ["card-wrapper"] },
+    cardContainer
+  );
 
   return HTMLCard;
-} 
+}
+//debugger
 cardsContainer.append(...HTMLCollectionActors);
 /* Creaters */
 
-function createElement(tag,{classNames, attributes}, ...children){
+function createElement(tag, { classNames}, ...children) {
   const element = document.createElement(tag);
-  if(classNames.length){
+  if (classNames.length) {
     element.classList.add(...classNames);
   }
-
-    for (const [nameAttr, valueAttr] of Object.entries(attributes)) {
-      element.setAttribute(nameAttr, valueAttr);
-    }
-
+  // for (const [nameAttr, valueAttr] of Object.entries(attributes)) {
+  //   element.setAttribute(nameAttr, valueAttr);
+  // }
   element.append(...children);
+  return element;
 }
 
-function createInitials({name}){
-  const cardInitials = document.createElement('div');
-  cardInitials.classList.add('card-initials');
-  cardInitials.style.backgroundColor = stringToColour(name || 'Anonim');
-  cardInitials.append(document.createTextNode(name[0] || 'NN'));
+function createWrapperPhoto(actor){
+  const cardPhotoWrapper = document.createElement('div');
+  cardPhotoWrapper.classList.add('card-photo-wrapper');
+  cardPhotoWrapper.setAttribute('id',`photo-wrapper${actor.id}`)
+  cardPhotoWrapper.append(createInitials(actor));
+  return cardPhotoWrapper;
+}
+
+function createInitials({ name }) {
+  const cardInitials = document.createElement("div");
+  cardInitials.classList.add("card-initials");
+  cardInitials.style.backgroundColor = stringToColour(name || "Anonim");
+  cardInitials.append(document.createTextNode(name[0] || "NN"));
   return cardInitials;
 }
 
-function createImage({photo, id, name}){
-  const cardPhoto = document.createElement('img');
-  cardPhoto.classList.add('card-photo');
-  cardPhoto.setAttribute('src', photo);
-  cardPhoto.setAttribute('alt', name || 'Anonim');
+function createImage({ photo, id, name }) {
+  const cardPhoto = document.createElement("img");
+  cardPhoto.classList.add("card-photo");
+  cardPhoto.setAttribute("src", photo);
+  cardPhoto.setAttribute("alt", name || "Anonim");
   cardPhoto.dataset.wrapperId = `photo-wrapper${id}`;
-  cardPhoto.addEventListener('error', handlePhotoError);
-  cardPhoto.addEventListener('load', handlePhotoLoad);
+  cardPhoto.addEventListener("error", handlePhotoError);
+  cardPhoto.addEventListener("load", handlePhotoLoad);
   return cardPhoto;
 }
 
 /* Handles */
-function handlePhotoError({target}){
+function handlePhotoError({ target }) {
   target.remove();
 }
-function handlePhotoLoad({target}){
+function handlePhotoLoad({ target }) {
   document.getElementById(target.dataset.wrapperId).append(target);
 }
 /* Tooltips */
@@ -72,10 +93,10 @@ function stringToColour(str) {
   for (let i = 0; i < str.length; i++) {
     hash = str.charCodeAt(i) + ((hash << 5) - hash);
   }
-  let colour = '#';
+  let colour = "#";
   for (let i = 0; i < 3; i++) {
-    const value = (hash >> (i * 8)) & 0xFF;
-    colour += ('00' + value.toString(16)).slice(-2);
+    const value = (hash >> (i * 8)) & 0xff;
+    colour += ("00" + value.toString(16)).slice(-2);
   }
   return colour;
 }
