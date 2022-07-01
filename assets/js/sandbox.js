588ef0a3f45b514238bc89906d12c10e79ba7473
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-01 16:03:06 +0300

    Example use method test

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,14 +1,13 @@
 "use strict";
-/*
-написать функцию  createAdder(n)
-которая возвращает другую функцию
-возвращенная функция должна принимать 
-аргумент m  
-и прибавлять его к n
-*/
-const createAdder = (n) => (m) => (n += m);
-//debugger
-const adder = createAdder(10);
-console.log(adder(4));
-console.log(adder(4));
-console.log(adder(4));
+
+const pattern1 = new RegExp('\\b[A-Z][a-z]{2,12}\\b','gi');
+
+const pattern2 = /^[0-9]{2,12}\b/g;
+
+const str = '788.All 234234 my trouble 342 seemed so 432 far away';
+
+if(pattern2.test(str)){
+  console.log(pattern2)
+}else{
+  console.log(str)
+}
