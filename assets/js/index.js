82ba911c64c3abbe4effd3537ad3682687b3e41b
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-01 15:50:20 +0300

    Add form

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,33 @@
 'use strict';
+const messages = [];
+const form = document.getElementById('formId');
+const messagesContainer = document.getElementById('messages');
+
+form.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  const testValue = e.target.test.value.trim();
+  if( !messages.includes(testValue) && testValue ){
+    messages.push(testValue);     
+    messagesContainer.append(createElement('p',{events:{'click':handleClickMessage}},testValue));
+  }
+  e.target.reset();
+})
+
+function handleClickMessage({target}){
+  target.style.backgroundColor = 'pink';
+}
+
+function createElement(tag, { classNames = [], attributes = {}, events={} }, ...children) {
+  const element = document.createElement(tag);
+  if (classNames.length) {
+    element.classList.add(...classNames);
+  }
+  for (const [nameAttr, valueAttr] of Object.entries(attributes)) {
+    element.setAttribute(nameAttr, valueAttr);
+  }
+  for (const [typeEvent, handleEvent] of Object.entries(events)) {
+    element.addEventListener(typeEvent, handleEvent);
+  }
+  element.append(...children);
+  return element;
+}
