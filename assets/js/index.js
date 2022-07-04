ee0ae5ddc6eaadd47698c6cd74ada4ec90190aae
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-04 15:58:08 +0300

    Example setTimeout/setInterval

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,46 +1,20 @@
-'use strict';
-const messages = [];
-const form = document.getElementById('formId');
-const messagesContainer = document.getElementById('messages');
+"use strict";
 
-form.addEventListener('submit', (e)=>{
-  e.preventDefault();
-  const testValue = e.target.test.value.trim();
-  const pattern = /^[A-Z][a-z]{2,12}$/;
-  //files with extends: jpg, png, webp, JPG, PNG, WEBP
-  //const pattern = /^.+\.(jpg|png|webp)$/i;
-  if( !messages.includes(testValue) && pattern.test(testValue) ){
-    messages.push(testValue);
-    const pElem = createElement('p',{events:{'click':handleClickMessage}},testValue) ;  
-    const btn = createElement('button',{},'x') ; 
-    pElem.append(btn);  
-    messagesContainer.append(pElem);
-  }
-  e.target.reset();
-})
+// const btn = document.getElementById('btn');
+// btn.addEventListener('click', btnHandler);
 
-//добавить кнопку удаления для каждого абзаца с именем
-//по клику на которую удaлять весь абзац со страницы
-//удалять значение из массива
+// function btnHandler(){
+//   //while(true){}
+// }
 
-function handleClickMessage({target}){
-  for (const p of document.getElementsByTagName('p')) {
-    p.style.backgroundColor = '';
-  }
-  target.style.backgroundColor = 'pink';
-}
+// console.log(1);
+// setTimeout(() => {
+//   console.log(3);
+// }, 0);
+// console.log(2);
 
-function createElement(tag, { classNames = [], attributes = {}, events={} }, ...children) {
-  const element = document.createElement(tag);
-  if (classNames.length) {
-    element.classList.add(...classNames);
-  }
-  for (const [nameAttr, valueAttr] of Object.entries(attributes)) {
-    element.setAttribute(nameAttr, valueAttr);
-  }
-  for (const [typeEvent, handleEvent] of Object.entries(events)) {
-    element.addEventListener(typeEvent, handleEvent);
-  }
-  element.append(...children);
-  return element;
-}
+const identificator = setInterval(() => {
+  console.log(3);
+}, 500);
+
+//clearInterval(identificator);
