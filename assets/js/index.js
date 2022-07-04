4d0904c75de95797e5b0aee92b5bea4274e25c7f
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-04 14:27:34 +0300

    Add button

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,17 +6,27 @@ const messagesContainer = document.getElementById('messages');
 form.addEventListener('submit', (e)=>{
   e.preventDefault();
   const testValue = e.target.test.value.trim();
-  //const pattern = /^[A-Z][a-z]{2,12} [A-Z]\.$/;
+  const pattern = /^[A-Z][a-z]{2,12}$/;
   //files with extends: jpg, png, webp, JPG, PNG, WEBP
-  const pattern = /^.+\.(jpg|png|webp)$/i;
+  //const pattern = /^.+\.(jpg|png|webp)$/i;
   if( !messages.includes(testValue) && pattern.test(testValue) ){
-    messages.push(testValue);     
-    messagesContainer.append(createElement('p',{events:{'click':handleClickMessage}},testValue));
+    messages.push(testValue);
+    const pElem = createElement('p',{events:{'click':handleClickMessage}},testValue) ;  
+    const btn = createElement('button',{},'x') ; 
+    pElem.append(btn);  
+    messagesContainer.append(pElem);
   }
   e.target.reset();
 })
 
+//добавить кнопку удаления для каждого абзаца с именем
+//по клику на которую удaлять весь абзац со страницы
+//удалять значение из массива
+
 function handleClickMessage({target}){
+  for (const p of document.getElementsByTagName('p')) {
+    p.style.backgroundColor = '';
+  }
   target.style.backgroundColor = 'pink';
 }
 
