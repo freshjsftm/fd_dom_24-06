749faca3e1629ff7b483eda765fcfd3f18273596
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-05 16:34:52 +0300

    Show data

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,12 @@
 "use strict";
 
 fetch('./assets/js/data.json')
-  .then((response)=>response.json(), 
-    (error)=>console.log(error))
-  .then((data)=>console.table(data),
-  (error)=>console.log(error))
+  .then((response)=>response.json())
+  .then((data)=>{
+    console.log(data.map((item)=>item.name).join(', '))
+    
+  })
+  .catch((error)=>console.log(error))
+  .finally(()=>console.log('finally'))
+
+
