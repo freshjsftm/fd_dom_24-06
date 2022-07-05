77af5014070d8ebdfb434eaf3538897a37cdacc5
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-05 16:58:35 +0300

    Add create myFirstPromise

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,35 @@
 "use strict";
 
-fetch('./assets/js/data.json')
-  .then((response)=>response.json())
-  .then((data)=>{
-    console.log(data.map((item)=>item.name).join(', '))
-    
+const myFirstPromise = new Promise(executor);
+function executor(resolve, reject){
+  Math.random()>0.5? resolve(12) : reject('error');
+}
+//console.log(myFirstPromise)
+myFirstPromise
+.then((data)=>console.log('resolve:', data))
+.catch((error)=>console.log('rejext:', error))
+
+
+function timeout(ms){
+  return new Promise((resolve, reject)=>{
+    setTimeout(()=>{Math.random()>0.5? resolve(12) : reject(0)}, ms)
   })
-  .catch((error)=>console.log(error))
-  .finally(()=>console.log('finally'))
+}
+timeout(5000)
+  .then((d)=>console.log(d))
+  .catch((err)=>console.log(err))
+  .finally(()=>console.log(3))
+
+
+
+
+// fetch('./assets/js/data.json')
+//   .then((response)=>response.json())
+//   .then((data)=>{
+//     console.log(data.map((item)=>item.name).join(', '))
+    
+//   })
+//   .catch((error)=>console.log(error))
+//   .finally(()=>console.log('finally'))
 
 
