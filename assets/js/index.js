41695831f592931de8cd6a34fec25d3c2b240b15
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-05 16:06:13 +0300

    Example p.then(onFulfilled, onRejected);

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,7 @@
 "use strict";
 
-// const promise1 = fetch('./assets/js/data.json');
-// console.log(promise1)
-
-// promise1.then((response)=>{
-//   console.log(response)
-//   const dataJson = response.json();
-//   console.log(dataJson)
-//   dataJson.then((data)=>{
-//     console.table(data)
-//   })
-// })
-
 fetch('./assets/js/data.json')
-  .then((response)=>{console.log(response); 
-    return response.json()})
-  .then((data)=>console.table(data))
+  .then((response)=>response.json(), 
+    (error)=>console.log(error))
+  .then((data)=>console.table(data),
+  (error)=>console.log(error))
