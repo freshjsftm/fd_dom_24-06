5b07d172068be20c720734cc014d108cbe9930ef
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-06 14:25:02 +0300

    Example work with errors

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,22 @@
 "use strict";
 
-const myFirstPromise = new Promise(executor);
-function executor(resolve, reject){
-  Math.random()>0.5? resolve(12) : reject('error');
-}
-//console.log(myFirstPromise)
-myFirstPromise
-.then((data)=>console.log('resolve:', data))
-.catch((error)=>console.log('rejext:', error))
-
-
-function timeout(ms){
-  return new Promise((resolve, reject)=>{
-    setTimeout(()=>{Math.random()>0.5? resolve(12) : reject(0)}, ms)
-  })
-}
-timeout(5000)
-  .then((d)=>console.log(d))
-  .catch((err)=>console.log(err))
-  .finally(()=>console.log(3))
-
-
-
-
-// fetch('./assets/js/data.json')
-//   .then((response)=>response.json())
-//   .then((data)=>{
-//     console.log(data.map((item)=>item.name).join(', '))
-    
-//   })
-//   .catch((error)=>console.log(error))
-//   .finally(()=>console.log('finally'))
-
+fetch('./assets/js/data.json')
+.then((response)=>response.json())
+.then((data)=>{
+  console.log(data);
+  if(data.length>2){
+    throw new Error('must be two')
+  }
+})
+.catch((error)=>{
+  document.body.append('500')
+  if(error instanceof TypeError){
+    console.log(error.message, ', check the path!')
+  }else if(error instanceof SyntaxError){
+    console.log(error.message, ', check the coma!')
+  }else{
+    console.log(error.message, '!!!!')
+  }
+})
+.finally(()=>{console.log('finally')})
 
